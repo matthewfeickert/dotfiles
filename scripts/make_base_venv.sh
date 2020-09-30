f1e123c1e3747a07a2f71687ed9776177e970684
Matthew Feickert <matthew.feickert@cern.ch>
2020-09-29 23:59:39 -0500

    Add script to setup base virtual environment

---
+++ scripts/make_base_venv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "${HOME}/.venvs/base" ]]; then
+  mkdir -p "${HOME}/.venvs"
+  cd "${HOME}/.venvs"
+  python3 -m venv base
+fi
+
+if [[ -d "${HOME}/.venvs/base" ]]; then
+  source "${HOME}/.venvs/base/bin/activate"
+  python -m pip install --quiet --upgrade pip setuptools wheel
+  python -m pip install --quiet \
+    numpy \
+    scipy \
+    pandamonium
+fi
