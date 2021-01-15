74297671576bf158decae87d0beef62d139628d8
Matthew Feickert <matthew.feickert@cern.ch>
2021-01-15 15:19:21 -0600

    Add nvidia-container-runtime
    Add PPA key retrival to setup.sh and add nvidia-container-runtime to
    apt.txt

--- setup.sh
+++ setup.sh
@@ -10,3 +10,17 @@ if [ ! -d "${HOME}/.local/share/fonts/truetype/felipa" ]; then
     fi
     fc-cache --force --verbose # force system font cache rebuild
 fi
+
+# Get keys for nvidia-container-runtime
+# c.f. https://nvidia.github.io/nvidia-container-runtime/
+# N.B.: Remember to: sudo systemctl restart docker
+if [ ! -f /etc/apt/sources.list.d/nvidia-container-runtime.list ]; then
+    sudo apt-get update
+    curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | \
+        sudo apt-key add -
+    distribution="$(. /etc/os-release;echo "${ID}${VERSION_ID}")"
+    curl -s -L "https://nvidia.github.io/nvidia-container-runtime/${distribution}/nvidia-container-runtime.list" | \
+        sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
+    sudo apt-get update
+    unset distribution
+fi
